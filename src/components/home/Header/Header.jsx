7165d55fbb058168abef1963271abecf369e0745
Yurii <fanfjrfj589@gmail.com>
2023-06-09 20:52:40 +0200

    header done + mixins (Code optimization)

--- src/components/home/Header/Header.jsx
+++ src/components/home/Header/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Logo from '../../../assets/Svg/Logo/Logo';
+import "../Header/Header.css"
 
 function Header() {
+    const navigation = ["Characters", "Episodes", "Location", "My watch list"];
+    
   return (
-	<div>Header</div>
+	<div className='header'>
+    <Logo />
+
+    <div className="nav">
+          {navigation.map(todo => <p key={todo} className="nav__text">{todo}</p>)
+          }
+		</div>
+  </div>
   )
 }
 
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./style/Home.css"
+import Header from "./Header/Header"
 
 function Home() {
   return (
 	<div>
-		
+		<Header />
 	</div>
   )
 }
